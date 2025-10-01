ae3b84e010a61455a047ad04a73417c3d5f2b554
rohit <rohitpachauri2@gmail.com>
2025-10-01 07:17:20 +0530

    heello

--- src/test/java/TestCases/CheckoutTestCase.java
+++ src/test/java/TestCases/CheckoutTestCase.java
@@ -2,21 +2,15 @@ package TestCases;
 
 import java.time.Duration;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import Pages.BaseTest;
 import Pages.CartPage;
 import Pages.CheckoutPage;
 import Pages.Loginpage;
 import Utility.ReadExcelFile;
-
-import java.io.File;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
-import org.testng.ITestListener;
-import org.testng.ITestResult;
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
@@ -25,37 +19,33 @@ import com.aventstack.extentreports.markuputils.MarkupHelper;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
-@Test
 public class CheckoutTestCase extends BaseTest {
     String filepath = "C:\\Users\\PC\\git\\repository2\\Testdata\\Book1.xlsx";
+    private ExtentReports reports;
+    private ExtentTest test;
+    private WebDriver driver;
 
-    public void checkoutcase() throws InterruptedException {
-    	
-    	private ExtentSparkReporter htmlReporter;
-        private ExtentReports reports;
-        private ExtentTest test;
-        private WebDriver driver;  // Make sure WebDriver is properly passed.
-
-        // Configuring the report
-        public void configureReport1() {
-        	htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/ExtentListenerReportDemo2.html");
-
-            reports = new ExtentReports();
-            reports.attachReporter(htmlReporter);
-
-            // Add system information/environment info to reports
-            reports.setSystemInfo("Machine", "PC");
-            reports.setSystemInfo("OS", "Windows 10");
-
-            // Customize report details
-            htmlReporter.config().setDocumentTitle("Extent Listener Report Demo");
-            htmlReporter.config().setReportName("This is my secong Report");
-            htmlReporter.config().setTheme(Theme.DARK);
-        }
-        configureReport1();
+    // Initialize the Extent Reports and WebDriver
+    @BeforeClass
+    public void setup() {
+        // Configure Extent Report
+        ExtentSparkReporter htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/ExtentListenerReportDemo2.html");
+        reports = new ExtentReports();
+        reports.attachReporter(htmlReporter);
+        reports.setSystemInfo("Machine", "PC");
+        reports.setSystemInfo("OS", "Windows 10");
+        htmlReporter.config().setDocumentTitle("Extent Listener Report Demo");
+        htmlReporter.config().setReportName("Checkout Test Report");
+        htmlReporter.config().setTheme(Theme.DARK);
+        
+        // Initialize WebDriver (Assuming driver is initialized in BaseTest)
+        driver = getDriver(); // Ensure this method is available in BaseTest
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        Loginpage lp = new Loginpage(driver);
+    }
 
+    @Test
+    public void checkoutcase() throws InterruptedException {
+        Loginpage lp = new Loginpage(driver);
         int passedTests = 0;   // Counter for passed tests
         int failedTests = 0;   // Counter for failed tests
 
@@ -70,12 +60,7 @@ public class CheckoutTestCase extends BaseTest {
 
                 // Proceed if login is successful
                 CartPage cp = new CartPage(driver);
-                try {
-                    cp.addtocart();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    Assert.fail("Error while adding items to cart for user: " + username);
-                }
+                cp.addtocart();
 
                 CheckoutPage cp1 = new CheckoutPage(driver);
                 cp1.checkout();
@@ -83,18 +68,16 @@ public class CheckoutTestCase extends BaseTest {
 
                 System.out.println("Checkout completed successfully for: " + username);
                 lp.logout();
-                passedTests++;  // Increment passed test counter
-                test.log(Status.PASS, MarkupHelper.createLabel("Test case passed: " + username, ExtentColor.GREEN));
+
+                passedTests++;
+                test.log(Status.PASS, MarkupHelper.createLabel("Test case passed for user: " + username, ExtentColor.GREEN));
 
             } catch (AssertionError e) {
-                // Log the failure and continue with the next iteration
-                System.out.println("Test failed for user: " + username + " -> " + e.getMessage());
-                failedTests++;  // Increment failed test counter
-                test.log(Status.FAIL, MarkupHelper.createLabel("Test case passed: " + username, ExtentColor.RED));
+                failedTests++;
+                test.log(Status.FAIL, MarkupHelper.createLabel("Test case failed for user: " + username, ExtentColor.RED));
             }
 
-            // Optional: Adding a small wait to avoid rapid successive logins
-            Thread.sleep(1000);
+            Thread.sleep(1000); // Small wait to avoid rapid logins
         }
 
         // Log the summary after all tests are complete
@@ -102,6 +85,15 @@ public class CheckoutTestCase extends BaseTest {
         System.out.println("Total Tests: 4");
         System.out.println("Passed: " + passedTests);
         System.out.println("Failed: " + failedTests);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        // Finalize Extent Report
         reports.flush();
+        // Close WebDriver if needed
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
