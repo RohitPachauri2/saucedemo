e41214cc1896bfdf476130df193b198d82905f5b
rohit <rohitpachauri2@gmail.com>
2025-10-01 07:15:27 +0530

    new commit

--- src/test/java/TestCases/CheckoutTestCase.java
+++ src/test/java/TestCases/CheckoutTestCase.java
@@ -9,11 +9,50 @@ import Pages.CheckoutPage;
 import Pages.Loginpage;
 import Utility.ReadExcelFile;
 
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
 @Test
 public class CheckoutTestCase extends BaseTest {
     String filepath = "C:\\Users\\PC\\git\\repository2\\Testdata\\Book1.xlsx";
 
     public void checkoutcase() throws InterruptedException {
+    	
+    	private ExtentSparkReporter htmlReporter;
+        private ExtentReports reports;
+        private ExtentTest test;
+        private WebDriver driver;  // Make sure WebDriver is properly passed.
+
+        // Configuring the report
+        public void configureReport1() {
+        	htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/ExtentListenerReportDemo2.html");
+
+            reports = new ExtentReports();
+            reports.attachReporter(htmlReporter);
+
+            // Add system information/environment info to reports
+            reports.setSystemInfo("Machine", "PC");
+            reports.setSystemInfo("OS", "Windows 10");
+
+            // Customize report details
+            htmlReporter.config().setDocumentTitle("Extent Listener Report Demo");
+            htmlReporter.config().setReportName("This is my secong Report");
+            htmlReporter.config().setTheme(Theme.DARK);
+        }
+        configureReport1();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         Loginpage lp = new Loginpage(driver);
 
@@ -45,11 +84,13 @@ public class CheckoutTestCase extends BaseTest {
                 System.out.println("Checkout completed successfully for: " + username);
                 lp.logout();
                 passedTests++;  // Increment passed test counter
+                test.log(Status.PASS, MarkupHelper.createLabel("Test case passed: " + username, ExtentColor.GREEN));
 
             } catch (AssertionError e) {
                 // Log the failure and continue with the next iteration
                 System.out.println("Test failed for user: " + username + " -> " + e.getMessage());
                 failedTests++;  // Increment failed test counter
+                test.log(Status.FAIL, MarkupHelper.createLabel("Test case passed: " + username, ExtentColor.RED));
             }
 
             // Optional: Adding a small wait to avoid rapid successive logins
@@ -61,5 +102,6 @@ public class CheckoutTestCase extends BaseTest {
         System.out.println("Total Tests: 4");
         System.out.println("Passed: " + passedTests);
         System.out.println("Failed: " + failedTests);
+        reports.flush();
     }
 }
